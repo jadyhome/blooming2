8cc5b237d57ad31ee0c73dbc173f105c30551112
Jady Hom <hyjady@gmail.com>
2021-06-10 12:53:59 -0400

    created BeigeNav.js -- for beige colored navbar

---
+++ src/components/BeigeNav.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../styles/Nav.css";
+
+const BeigeNav = () => {
+  return (
+    <header className="beige-navbar">
+      <nav className="beige-nav">
+        <NavLink to="/plants" className="b-plant">
+          Plants
+        </NavLink>
+
+        <NavLink to="/supplies" className="b-supplies">
+          Supplies
+        </NavLink>
+
+        <NavLink to="/merch" className="b-merch">
+          Merch
+        </NavLink>
+
+        <NavLink to="/" className="b-title">
+          blooming
+        </NavLink>
+
+        <NavLink to="/blog" className="b-blog">
+          Blog
+        </NavLink>
+
+        <NavLink to="/faq" className="b-faq">
+          FAQ
+        </NavLink>
+
+        <NavLink to="/cart" className="b-cart">
+          Cart
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
+
+export default BeigeNav;
