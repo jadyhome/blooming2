949e40f338e24fe92eebdaf8753fdca035fb0199
Jady Hom <hyjady@gmail.com>
2021-06-10 19:51:04 -0400

    created plantpreview section for mainpage

---
+++ src/components/PlantPreview.js
@@ -0,0 +1,72 @@
+import React from "react";
+import "../styles/Main.css";
+
+const PlantPreview = () => {
+  return (
+    <div className="plantspreview">
+      <div className="plants-container">
+        <div className="plant">
+          <div className="plantimg1">
+            <img
+              src="https://images.pexels.com/photos/4507748/pexels-photo-4507748.jpeg?cs=srgb&dl=pexels-cottonbro-4507748.jpg&fm=jpg"
+              alt="Green Succulent Plant on Brown Clay Pot"
+              className="pimg1"
+            />
+          </div>
+        </div>
+
+        <div className="plant">
+          <div className="plantimg2">
+            <img
+              src="https://images.pexels.com/photos/4503747/pexels-photo-4503747.jpeg?cs=srgb&dl=pexels-cottonbro-4503747.jpg&fm=jpg"
+              alt="Person Holding Potted Plant"
+              className="pimg2"
+            />
+          </div>
+        </div>
+
+        <div className="plant">
+          <div className="plantimg3">
+            <img
+              src="https://images.pexels.com/photos/4505144/pexels-photo-4505144.jpeg?cs=srgb&dl=pexels-cottonbro-4505144.jpg&fm=jpg"
+              alt="Green Cactus Plant on Brown Clay Pot"
+              className="pimg3"
+            />
+          </div>
+        </div>
+
+        <div className="plant">
+          <div className="plantimg4">
+            <img
+              src="https://images.pexels.com/photos/4690294/pexels-photo-4690294.jpeg?cs=srgb&dl=pexels-cottonbro-4690294.jpg&fm=jpg"
+              alt="Green and Red Plant on White Ceramic Pot"
+              className="pimg4"
+            />
+          </div>
+        </div>
+
+        <div className="plant">
+          <div className="plantimg5">
+            <img
+              src="https://images.pexels.com/photos/4503271/pexels-photo-4503271.jpeg?cs=srgb&dl=pexels-cottonbro-4503271.jpg&fm=jpg"
+              alt="Potted Plant"
+              className="pimg5"
+            />
+          </div>
+        </div>
+
+        <div className="plant">
+          <div className="plantimg5">
+            <img
+              src="https://images.pexels.com/photos/4503751/pexels-photo-4503751.jpeg?cs=srgb&dl=pexels-cottonbro-4503751.jpg&fm=jpg"
+              alt="Person Holding Green Plant in Brown Pot"
+              className="pimg5"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PlantPreview;
