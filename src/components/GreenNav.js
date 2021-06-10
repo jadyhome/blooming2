15dcaa9d190e0657af57b07b2219d4f736ad0442
Jady Hom <hyjady@gmail.com>
2021-06-10 12:53:47 -0400

    changed Nav.js to GreenNav.js (for pages w/ green navbar)

---
+++ src/components/GreenNav.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../styles/Nav.css";
+
+const GreenNav = () => {
+  return (
+    <header className="green-navbar">
+      <nav className="green-nav">
+        <NavLink to="/plants" className="g-plant">
+          Plants
+        </NavLink>
+
+        <NavLink to="/supplies" className="g-supplies">
+          Supplies
+        </NavLink>
+
+        <NavLink to="/merch" className="g-merch">
+          Merch
+        </NavLink>
+
+        <NavLink to="/" className="g-title">
+          blooming
+        </NavLink>
+
+        <NavLink to="/blog" className="g-blog">
+          Blog
+        </NavLink>
+        
+        <NavLink to="/faq" className="g-faq">
+          FAQ
+        </NavLink>
+
+        <NavLink to="/cart" className="g-cart">
+          Cart
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
+
+export default GreenNav;
