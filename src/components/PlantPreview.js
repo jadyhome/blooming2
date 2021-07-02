9c43d34bc688b91661b13e0d5b6ff997e6946924
Jady Hom <hyjady@gmail.com>
2021-07-02 18:33:33 -0400

    added credits to photo for alt tags

--- src/components/PlantPreview.js
+++ src/components/PlantPreview.js
@@ -9,7 +9,7 @@ const PlantPreview = () => {
           <div className="plantimg1">
             <img
               src="https://images.pexels.com/photos/4507748/pexels-photo-4507748.jpeg?cs=srgb&dl=pexels-cottonbro-4507748.jpg&fm=jpg"
-              alt="Green Succulent Plant on Brown Clay Pot"
+              alt="Green Succulent Plant on Brown Clay Pot - Credits: https://www.pexels.com/@cottonbro"
               className="pimg1"
             />
           </div>
@@ -19,7 +19,7 @@ const PlantPreview = () => {
           <div className="plantimg2">
             <img
               src="https://images.pexels.com/photos/4503747/pexels-photo-4503747.jpeg?cs=srgb&dl=pexels-cottonbro-4503747.jpg&fm=jpg"
-              alt="Person Holding Potted Plant"
+              alt="Person Holding Potted Plant - Credits: https://www.pexels.com/@cottonbro"
               className="pimg2"
             />
           </div>
@@ -29,7 +29,7 @@ const PlantPreview = () => {
           <div className="plantimg3">
             <img
               src="https://images.pexels.com/photos/4505144/pexels-photo-4505144.jpeg?cs=srgb&dl=pexels-cottonbro-4505144.jpg&fm=jpg"
-              alt="Green Cactus Plant on Brown Clay Pot"
+              alt="Green Cactus Plant on Brown Clay Pot - Credits: https://www.pexels.com/@cottonbro"
               className="pimg3"
             />
           </div>
@@ -39,7 +39,7 @@ const PlantPreview = () => {
           <div className="plantimg4">
             <img
               src="https://images.pexels.com/photos/4690294/pexels-photo-4690294.jpeg?cs=srgb&dl=pexels-cottonbro-4690294.jpg&fm=jpg"
-              alt="Green and Red Plant on White Ceramic Pot"
+              alt="Green and Red Plant on White Ceramic Pot - Credits: https://www.pexels.com/@cottonbro"
               className="pimg4"
             />
           </div>
@@ -49,7 +49,7 @@ const PlantPreview = () => {
           <div className="plantimg5">
             <img
               src="https://images.pexels.com/photos/4503271/pexels-photo-4503271.jpeg?cs=srgb&dl=pexels-cottonbro-4503271.jpg&fm=jpg"
-              alt="Potted Plant"
+              alt="Potted Plant - Credits: https://www.pexels.com/@cottonbro"
               className="pimg5"
             />
           </div>
@@ -59,7 +59,7 @@ const PlantPreview = () => {
           <div className="plantimg5">
             <img
               src="https://images.pexels.com/photos/4503751/pexels-photo-4503751.jpeg?cs=srgb&dl=pexels-cottonbro-4503751.jpg&fm=jpg"
-              alt="Person Holding Green Plant in Brown Pot"
+              alt="Person Holding Green Plant in Brown Pot - Credits: https://www.pexels.com/@cottonbro"
               className="pimg5"
             />
           </div>
