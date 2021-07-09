c3150164be548a5881ede3b69ed602f141c4acd9
Jady Hom <hyjady@gmail.com>
2021-07-09 19:03:14 -0400

    created blog section for blog page

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/Global.css";
+import "../styles/Blog.css";
 import BeigeNav from "../components/BeigeNav";
 import BeigeFooter from "../components/BeigeFooter";
 
@@ -7,6 +8,185 @@ const BlogPage = () => {
   return (
     <div className="blogpage">
       <BeigeNav />
+
+      <div className="blog-container">
+        <div className="blog-info">
+          <img
+            src="https://images.pexels.com/photos/4503744/pexels-photo-4503744.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+            alt="Green Plant on White Ceramic Pot - Credits: https://www.pexels.com/@cottonbro"
+            className="blog-pic"
+          />
+          <h4 className="details1">Details of Plant Here:</h4>
+          <p className="details2">
+            <em>Common Name</em> <br /> ZZ Plant
+            <br />
+            <br />
+            <em>Scientific Name</em> <br /> Zamioculcas Zamiifolia
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+            <br />
+            <br />
+            <em>Lighting</em> <br /> Low to Bright Indirect Light
+            <br />
+            <br />
+            <em>Watering</em> <br /> Water every 1 - 2 weeks or when soil is
+            nearly dried <br />
+            <br />
+            <em>Pet-Friendly</em> <br /> Toxic to pets if ingested
+            <br />
+            <br />
+            <em>Maintenance Level</em> <br /> Low
+          </p>
+        </div>
+
+        <div className="blog-section">
+          <h1 className="title">LOREM IPSUM DOLOR</h1>
+          <h3 className="by">by Zamioculcas Zamiifolia - February 21, 2021</h3>
+          <p className="blog">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et
+            malesuada fames ac. Elit at imperdiet dui accumsan sit amet nulla
+            facilisi. Consequat mauris nunc congue nisi vitae. Adipiscing at in
+            tellus integer feugiat scelerisque varius morbi. Et molestie ac
+            feugiat sed lectus vestibulum mattis. Turpis egestas pretium aenean
+            pharetra magna ac placerat. Ullamcorper dignissim cras tincidunt
+            lobortis feugiat vivamus at augue eget. At volutpat diam ut
+            venenatis. Molestie at elementum eu facilisis sed odio morbi. Cras
+            ornare arcu dui vivamus arcu felis bibendum ut. Adipiscing elit
+            pellentesque habitant morbi tristique. Bibendum enim facilisis
+            gravida neque convallis a cras semper. Ac turpis egestas integer
+            eget aliquet nibh. Aliquam vestibulum morbi blandit cursus risus at
+            ultrices mi tempus. Dui sapien eget mi proin. Massa tincidunt nunc
+            pulvinar sapien et ligula ullamcorper malesuada. Sit amet porttitor
+            eget dolor morbi non. Aliquam faucibus purus in massa tempor nec
+            feugiat nisl pretium. Amet massa vitae tortor condimentum lacinia
+            quis vel.
+            <br />
+            <br />
+            Tempus egestas sed sed risus pretium quam. Tempus urna et pharetra
+            pharetra massa massa ultricies mi quis. Sed turpis tincidunt id
+            aliquet risus. Sit amet venenatis urna cursus eget nunc scelerisque.
+            Nisl rhoncus mattis rhoncus urna neque viverra justo. Porttitor leo
+            a diam sollicitudin. Pellentesque habitant morbi tristique senectus
+            et netus. Augue lacus viverra vitae congue eu consequat ac felis
+            donec. Facilisi etiam dignissim diam quis enim. Massa enim nec dui
+            nunc mattis. Cursus metus aliquam eleifend mi. Amet consectetur
+            adipiscing elit duis tristique. Pharetra sit amet aliquam id diam
+            maecenas ultricies. Mi proin sed libero enim sed faucibus.
+            <br />
+            <br />
+            Eget mauris pharetra et ultrices neque ornare aenean. Velit euismod
+            in pellentesque massa placerat duis. Sed velit dignissim sodales ut
+            eu. Enim sit amet venenatis urna cursus eget nunc. Feugiat vivamus
+            at augue eget arcu. Sit amet mattis vulputate enim nulla. Sagittis
+            id consectetur purus ut faucibus pulvinar elementum integer enim.
+            Porttitor lacus luctus accumsan tortor posuere ac ut. Vitae suscipit
+            tellus mauris a diam. In tellus integer feugiat scelerisque varius
+            morbi enim. Vulputate enim nulla aliquet porttitor lacus luctus
+            accumsan tortor. Amet nisl suscipit adipiscing bibendum est
+            ultricies integer quis auctor. Morbi tristique senectus et netus et
+            malesuada fames ac turpis. Neque aliquam vestibulum morbi blandit
+            cursus risus. Sed felis eget velit aliquet sagittis id consectetur.
+            Dignissim cras tincidunt lobortis feugiat. Varius quam quisque id
+            diam vel quam elementum. Mauris in aliquam sem fringilla ut morbi
+            tincidunt augue.
+            <br />
+            <br />
+            Ac turpis egestas maecenas pharetra convallis. Mattis aliquam
+            faucibus purus in massa tempor nec feugiat. Odio tempor orci dapibus
+            ultrices. Viverra adipiscing at in tellus integer. Pellentesque nec
+            nam aliquam sem. Pellentesque nec nam aliquam sem et tortor
+            consequat id porta. Libero id faucibus nisl tincidunt eget. Felis
+            bibendum ut tristique et egestas quis ipsum. Nec ultrices dui sapien
+            eget. Imperdiet dui accumsan sit amet nulla. In hendrerit gravida
+            rutrum quisque non tellus orci. Cursus sit amet dictum sit amet
+            justo. Id donec ultrices tincidunt arcu non sodales. Cras tincidunt
+            lobortis feugiat vivamus at. Sed arcu non odio euismod.
+            <br />
+            <br />
+            Orci phasellus egestas tellus rutrum tellus pellentesque eu
+            tincidunt. Condimentum vitae sapien pellentesque habitant morbi
+            tristique senectus et. Sed enim ut sem viverra aliquet eget sit
+            amet. Commodo viverra maecenas accumsan lacus vel facilisis
+            volutpat. Enim ut tellus elementum sagittis vitae et leo duis ut. Et
+            sollicitudin ac orci phasellus egestas tellus rutrum tellus. Auctor
+            elit sed vulputate mi. A scelerisque purus semper eget duis. Laoreet
+            suspendisse interdum consectetur libero id faucibus nisl. Pharetra
+            convallis posuere morbi leo urna molestie at elementum. Porta nibh
+            venenatis cras sed felis eget velit. At urna condimentum mattis
+            pellentesque id nibh. Et netus et malesuada fames ac turpis. Amet
+            tellus cras adipiscing enim. Rutrum tellus pellentesque eu tincidunt
+            tortor.
+            <br />
+            <br />
+            Duis at consectetur lorem donec massa sapien faucibus et. At quis
+            risus sed vulputate odio ut enim. Leo vel fringilla est ullamcorper
+            eget. Eu consequat ac felis donec et odio pellentesque. Diam
+            phasellus vestibulum lorem sed risus ultricies tristique nulla
+            aliquet. Tempus iaculis urna id volutpat lacus. Tellus at urna
+            condimentum mattis pellentesque. Nunc faucibus a pellentesque sit
+            amet. Massa massa ultricies mi quis hendrerit dolor magna. Amet
+            consectetur adipiscing elit duis tristique sollicitudin. Ante metus
+            dictum at tempor commodo. Sed enim ut sem viverra. Lectus vestibulum
+            mattis ullamcorper velit sed ullamcorper morbi tincidunt.
+            <br />
+            <br />
+            Non consectetur a erat nam at lectus urna duis convallis. Eget sit
+            amet tellus cras adipiscing enim. Velit sed ullamcorper morbi
+            tincidunt ornare massa. Nunc scelerisque viverra mauris in aliquam
+            sem fringilla. Massa vitae tortor condimentum lacinia. Ut lectus
+            arcu bibendum at varius vel pharetra. Sit amet consectetur
+            adipiscing elit. Et egestas quis ipsum suspendisse ultrices gravida
+            dictum. Venenatis a condimentum vitae sapien pellentesque habitant
+            morbi. Sed odio morbi quis commodo. Quisque sagittis purus sit amet
+            volutpat consequat mauris nunc congue. Sit amet nulla facilisi morbi
+            tempus iaculis urna id volutpat. Vulputate enim nulla aliquet
+            porttitor lacus luctus.
+            <br />
+            <br />
+            Senectus et netus et malesuada fames ac turpis egestas maecenas.
+            Quis imperdiet massa tincidunt nunc pulvinar. Eget sit amet tellus
+            cras adipiscing enim eu. Dolor morbi non arcu risus quis varius
+            quam. Blandit aliquam etiam erat velit scelerisque in dictum. Et
+            malesuada fames ac turpis. At erat pellentesque adipiscing commodo.
+            Purus sit amet volutpat consequat mauris nunc congue nisi. Maecenas
+            sed enim ut sem viverra aliquet eget sit amet. Mattis vulputate enim
+            nulla aliquet porttitor lacus luctus accumsan. Mauris a diam
+            maecenas sed enim ut sem viverra. Nunc non blandit massa enim nec
+            dui.
+            <br />
+            <br />
+            Non diam phasellus vestibulum lorem. Ut placerat orci nulla
+            pellentesque. Egestas purus viverra accumsan in nisl. Quam viverra
+            orci sagittis eu volutpat odio facilisis mauris sit. Hendrerit dolor
+            magna eget est lorem ipsum. Aliquet risus feugiat in ante metus.
+            Elit at imperdiet dui accumsan sit. Amet nisl suscipit adipiscing
+            bibendum est ultricies integer. Eu scelerisque felis imperdiet proin
+            fermentum leo. Ante in nibh mauris cursus. Suspendisse potenti
+            nullam ac tortor. Volutpat ac tincidunt vitae semper quis. Nec nam
+            aliquam sem et tortor consequat. Elit pellentesque habitant morbi
+            tristique senectus et netus. Placerat duis ultricies lacus sed
+            turpis tincidunt.
+            <br />
+            <br />
+            Mi in nulla posuere sollicitudin. Amet tellus cras adipiscing enim.
+            Magna sit amet purus gravida quis blandit. Adipiscing diam donec
+            adipiscing tristique risus nec. Gravida quis blandit turpis cursus
+            in hac. Nulla at volutpat diam ut. Sed id semper risus in hendrerit
+            gravida. Nulla pellentesque dignissim enim sit. Sit amet consectetur
+            adipiscing elit ut aliquam. Dapibus ultrices in iaculis nunc sed
+            augue lacus. Ornare quam viverra orci sagittis eu volutpat odio. Eu
+            lobortis elementum nibh tellus molestie nunc non blandit.
+          </p>
+        </div>
+      </div>
 
       <BeigeFooter />
     </div>
